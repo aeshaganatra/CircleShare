f5dfd4cc08da4aaf204704bbe2caca9ff6eacf1d
Aesha Ganatra <40544440+aeshaganatra@users.noreply.github.com>
2023-12-28 11:34:54 -0500

    frontend and backend

--- circleshare_frontend/src/components/Pin.jsx
+++ circleshare_frontend/src/components/Pin.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { v4 as uuidv4 } from 'uuid';
 import { MdDownloadForOffline } from 'react-icons/md';
